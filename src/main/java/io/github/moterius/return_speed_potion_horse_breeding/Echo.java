a0f2043a06c622a71702d86563e7479e3ea395ed
BuildTools <unconfigured@null.spigotmc.org>
2022-03-15 20:34:51 +0100

    Implement Config Implement JumpHeight Slight refraction of already present Code

--- src/main/java/io/github/moterius/return_speed_potion_horse_breeding/Echo.java
+++ src/main/java/io/github/moterius/return_speed_potion_horse_breeding/Echo.java
@@ -1,7 +1,7 @@
 package io.github.moterius.return_speed_potion_horse_breeding;
 
-import org.bukkit.Bukkit;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
@@ -10,18 +10,42 @@ import org.bukkit.event.entity.EntityBreedEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffectType;
 
+import java.util.EnumSet;
+
 public class Echo extends JavaPlugin implements Listener {
+    static FileConfiguration config;
+
     @Override
     public void onEnable(){
         getServer().getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+        config = getConfig();
     }
 
     @EventHandler
     public void event(EntityBreedEvent e){
-        if(e.getFather().getType() == EntityType.HORSE &&
-        e.getMother().getType() == EntityType.HORSE){
-            LivingEntity a = e.getBreeder();
-            LivingEntity b = e.getEntity();
+        if(!config.getBoolean("allow_everything")){
+            EnumSet<EntityType> typeSet = EnumSet.noneOf(EntityType.class);
+            if(config.getBoolean("allow_donkeys"))
+                typeSet.add(EntityType.DONKEY);
+            if(config.getBoolean("allow_horses"))
+                typeSet.add(EntityType.HORSE);
+            if(config.getBoolean("allow_mules"))
+                typeSet.add(EntityType.MULE);
+            if(config.getBoolean("allow_pigs"))
+                typeSet.add(EntityType.PIG);
+            if(config.getBoolean("allow_strider"))
+                typeSet.add(EntityType.STRIDER);
+
+            if(! typeSet.contains(e.getEntity().getType()) )
+                return;
+        }
+
+        LivingEntity a = e.getBreeder();
+        LivingEntity b = e.getEntity();
+
+        if(config.getBoolean("effect_speed") &&
+                (a.hasPotionEffect(PotionEffectType.SPEED)||b.hasPotionEffect(PotionEffectType.SPEED))){
             double s_a = a.hasPotionEffect(PotionEffectType.SPEED) ?
                     a.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getValue() :
                     a.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getBaseValue();
@@ -32,5 +56,16 @@ public class Echo extends JavaPlugin implements Listener {
             e.getEntity().getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(avg);
         }
 
+        if(config.getBoolean("effect_jump") && e.getEntityType() == EntityType.HORSE &&
+                (a.hasPotionEffect(PotionEffectType.JUMP)||b.hasPotionEffect(PotionEffectType.JUMP))){
+            double s_a = a.hasPotionEffect(PotionEffectType.JUMP) ?
+                    a.getAttribute(Attribute.HORSE_JUMP_STRENGTH).getValue() :
+                    a.getAttribute(Attribute.HORSE_JUMP_STRENGTH).getBaseValue();
+            double s_b = b.hasPotionEffect(PotionEffectType.JUMP) ?
+                    b.getAttribute(Attribute.HORSE_JUMP_STRENGTH).getValue() :
+                    b.getAttribute(Attribute.HORSE_JUMP_STRENGTH).getBaseValue();
+            double avg = (s_a + s_b) / 2;
+            e.getEntity().getAttribute(Attribute.HORSE_JUMP_STRENGTH).setBaseValue(avg);
+        }
     }
 }
